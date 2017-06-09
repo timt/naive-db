c0f433cc6fffa802ca4836effd0823465f7d200b
Tim Tennant <ttennant@gmail.com>
2017-06-09 19:06:07 +0100

    Extrac Db to production class

---
+++ src/main/scala/Db.scala
@@ -0,0 +1,80 @@
+import java.nio.file.{Files, Paths}
+
+import io.circe.parser.parse
+import io.circe.syntax.EncoderOps
+import io.circe.{Decoder, Encoder, Json}
+
+import scala.io.Source
+import scala.reflect.io.File
+
+case class Db(values: Map[String, Json] = Map(), file: Option[String] = None) {
+
+  def set[T](item: String, value: T)(implicit encoder: Encoder[T]): Db = {
+    copy(values = values + (item -> value.asJson))
+      .save
+  }
+
+  def get[T](name: String)(implicit decoder: Decoder[T]): Option[T] = values.get(name).flatMap(_.as[T].toOption)
+
+  private def getList[T](listName: String)(implicit decoder: Decoder[T]): List[T] = get[List[T]](listName).getOrElse(List.empty[T])
+
+  def addItem[T](listName: String, item: T)(implicit encoder: Encoder[T], decoder: Decoder[T]): Db = set[List[T]](listName, (item :: getList(listName)).reverse)
+
+  def find[T](listName: String, predicate: T => Boolean)(implicit encoder: Encoder[T], decoder: Decoder[T]): Option[T] = get[List[T]](listName).flatMap(_.find(predicate))
+
+  def removeItem[T](listName: String, predicate: T => Boolean)(implicit encoder: Encoder[T], decoder: Decoder[T]): Db = {
+    set(listName, getList(listName).filterNot(predicate))
+  }
+
+  def remove(name: String): Db = {
+    copy(values = values.filterKeys(_ != name))
+      .save
+  }
+
+  private def save: Db = {
+    file.foreach(File(_).writeAll(values.asJson.spaces2))
+    this
+  }
+
+}
+
+object Db {
+
+  def initialize(file: String): Db = {
+    if (Files.exists(Paths.get(file)) && (Files.size(Paths.get(file)) > 0)) {
+      Db(parse(Source.fromFile(file)("UTF-8").mkString)
+        .getOrElse(throw new RuntimeException(s"Oops! Can not load file [$file]"))
+        .asObject.map(_.toMap)
+        .getOrElse(Map()),
+        Some(file))
+    } else Db(file = Some(file)).save
+  }
+
+  def inMemory: Db = new Db()
+
+
+  //  def load: Db = {
+  //      Db(parse(Source.fromFile(dbFileName)("UTF-8").mkString)
+  //        .getOrElse(throw new RuntimeException("boom!"))
+  //        .asObject.map(_.toMap)
+  //        .getOrElse(Map())
+  //      )
+  //  }
+
+  //  def set[A](itemName: String, value: A)(implicit encode: Encoder[A]): Unit =
+  //    File("db.json").writeAll(value.asJson.spaces2)
+
+  //  val file = File("db.json")
+  //
+  //  if (!file.exists || file.length == 0){
+  //    initialize
+  //  }
+  //
+  //  def initialize: Db = {
+  //    val asJson: Json = Map[String, String]().asJson
+  //    file.writeAll(asJson.spaces2)
+  //    this
+  //  }
+
+
+}
--- src/test/scala/JsonDbSpec.scala
+++ src/test/scala/JsonDbSpec.scala
@@ -2,14 +2,12 @@ import java.nio.file.{Files, Paths}
 
 import io.circe._
 import io.circe.generic.semiauto._
+import io.circe.parser.parse
 import io.circe.syntax.EncoderOps
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.io.Source
 import scala.reflect.io.File
-import io.circe.parser.parse
-
-import scala.collection.immutable.Seq
 
 
 class JsonDbSpec extends FreeSpec with Matchers {
@@ -181,86 +179,11 @@ class JsonDbSpec extends FreeSpec with Matchers {
       .get[List[Post]]("posts") shouldBe Some(List(Post(1, "some post"), Post(3, "yet another post")))
   }
 
-
-  "??? can create new db in /tmp with random file name" in {
-
-  }
-
-
 }
 
-case class Db(values: Map[String, Json] = Map(), file: Option[String] = None) {
-
-  private def save: Db = {
-    file.foreach(File(_).writeAll(values.asJson.spaces2))
-    this
-  }
-
-  def set[T](item: String, value: T)(implicit encoder: Encoder[T]): Db = {
-    copy(values = values + (item -> value.asJson))
-      .save
-  }
-
-  def get[T](name: String)(implicit decoder: Decoder[T]): Option[T] = values.get(name).flatMap(_.as[T].toOption)
-
-  private def getList[T](listName: String)(implicit decoder: Decoder[T]): List[T] = get[List[T]](listName).getOrElse(List.empty[T])
-
-  def addItem[T](listName: String, item: T)(implicit encoder: Encoder[T], decoder: Decoder[T]): Db = set[List[T]](listName, (item :: getList(listName)).reverse)
-
-  def find[T](listName: String, predicate: T => Boolean)(implicit encoder: Encoder[T], decoder: Decoder[T]): Option[T] = get[List[T]](listName).flatMap(_.find(predicate))
-
-  def removeItem[T](listName: String, predicate: T => Boolean)(implicit encoder: Encoder[T], decoder: Decoder[T]): Db = {
-    set(listName, getList(listName).filterNot(predicate))
-  }
-
-  def remove(name: String): Db = {
-    copy(values = values.filterKeys(_ != name))
-      .save
-  }
 
 
-}
 
-object Db {
-
-  def initialize(file: String): Db = {
-    if (Files.exists(Paths.get(file)) && (Files.size(Paths.get(file)) > 0)) {
-      Db(parse(Source.fromFile(file)("UTF-8").mkString)
-        .getOrElse(throw new RuntimeException(s"Oops! Can not load file [$file]"))
-        .asObject.map(_.toMap)
-        .getOrElse(Map()),
-        Some(file))
-    } else Db(file = Some(file)).save
-  }
-
-  def inMemory: Db = new Db()
-
-
-  //  def load: Db = {
-  //      Db(parse(Source.fromFile(dbFileName)("UTF-8").mkString)
-  //        .getOrElse(throw new RuntimeException("boom!"))
-  //        .asObject.map(_.toMap)
-  //        .getOrElse(Map())
-  //      )
-  //  }
-
-  //  def set[A](itemName: String, value: A)(implicit encode: Encoder[A]): Unit =
-  //    File("db.json").writeAll(value.asJson.spaces2)
-
-  //  val file = File("db.json")
-  //
-  //  if (!file.exists || file.length == 0){
-  //    initialize
-  //  }
-  //
-  //  def initialize: Db = {
-  //    val asJson: Json = Map[String, String]().asJson
-  //    file.writeAll(asJson.spaces2)
-  //    this
-  //  }
-
-
-}
 
 //Small library that can put, save/update query json file (CATS/Argonaut)
 /*
